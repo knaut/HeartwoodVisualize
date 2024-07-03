e4961bfe10f1c15d4704b4e57f28020357e02ffa
knaut <daniel@smallinvisiblemachines.com>
2024-07-02 22:41:10 -0700

    double rings with perspective

--- src/components/Ring/index.svelte
+++ src/components/Ring/index.svelte
@@ -1,36 +1,53 @@
 <script>
-  let constrain = 20;
+  let constrain = 2;
   let mouse = { x: 0, y: 0 }
-  let ring;
+
+  const rings = [
+    {
+      perspective: 200,
+      x: 0,
+      y: 0,
+      z: 0
+    },
+    {
+      perspective: 200,
+      x: 0,
+      y: 0,
+      z: 50
+    }
+  ]
 
   function transform(x, y, el) {
     let box = el.getBoundingClientRect();
     let calcX = -(y - box.y - (box.height / 2)) / constrain;
     let calcY = (x - box.x - (box.width / 2)) / constrain;
     
-    return "perspective(100px) "
-      + "   rotateX("+ calcX + "deg) "
-      + "   rotateY("+ calcY + "deg) ";
+    for (let i = 0; rings.length > i; i++) {
+      rings[i].x = calcX
+      rings[i].y = calcY
+    }
+
   }
 
-  let styleString;
 
   function handleMousemove(event) {
-    mouse.x = event.clientX;
-    mouse.y = event.clientY;
+    const { clientX, clientY, target } = event
 
-    // console.log(mouse)
-
-    styleString = transform(mouse.x, mouse.y, event.target)
+    transform( clientX, clientY, target )
   }
-
 
 </script>
 
 <div class="container" on:mousemove={handleMousemove}>
-  <div class="ring" bind:this={ring} style="
-    transform: {styleString}
-  "></div>  
+  {#each rings as {perspective, x, y, z}, i}
+    <div class="ring" id="{`ring-${i}`}" on:mousemove={handleMousemove} style="
+      transform: 
+        perspective({perspective}px) 
+        rotateX({x}deg) 
+        rotateY({y}deg)
+        translateZ({z}px);
+    "></div>
+  {/each}
 </div>
 
 
@@ -44,10 +61,13 @@
   }
 
   .ring {
-    border: 2px solid red;
+    border: 10px solid red;
     border-radius: 100%;
-    height: 100px;
-    width: 100px;
+    height: 200px;
+    width: 200px;
     position: absolute;
+  }
+  .ring:nth-child(2) {
+    border: 10px solid cyan;
   }
 </style>
