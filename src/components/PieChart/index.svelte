d11dff60d5f7df43bfdc16d45175441625572555
knaut <daniel@smallinvisiblemachines.com>
2024-07-08 20:12:06 -0700

    demo 4

--- src/components/PieChart/index.svelte
+++ src/components/PieChart/index.svelte
@@ -6,6 +6,10 @@
 
   onMount(() => {
 
+    d3.select('#demo4')
+      .append('g')
+      .attr('transform', 'translate(100, 100)');
+
     const data = [
       { startAngle: 0, endAngle: Math.PI/4 },
       { startAngle: Math.PI/4, endAngle: Math.PI/2 },
@@ -15,20 +19,18 @@
 
     const arcGen = d3.arc()
       .innerRadius(70)
-      .outerRadius(90)
-      .startAngle( 0 )
-      .endAngle( 2 * Math.PI  )
+      .outerRadius(90);
 
-    d3.select('#demo1')
-      .append('g')
-      .attr('transform', 'translate(100,100)');
-
-    d3.select('#demo1 g')
+    d3.select('#demo4 g')
+      .selectAll('path')
+      .data(data)
+      .enter()
       .append('path')
       .attr('d', arcGen)
       .attr('fill', 'pink')
       .attr('stroke', 'gray')
-      .attr('stroke-width', 1);
+      .attr('stroke-width', 1)
+
 
   })
 
@@ -38,6 +40,6 @@
 <div>
   Pie Chart
 
-  <svg id="demo1" width="200" height="200"></svg>
+  <svg id="demo4" width="200" height="200"></svg>
 </div>
 
